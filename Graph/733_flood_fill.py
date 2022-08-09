cca34437826a275af561c34d9f1a9be2a53d532a
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-08-09 13:29:03 +0600

    leetcode 733 solved.

---
+++ Graph/733_flood_fill.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        starting_pixel = image[sr][sc]
+        self.dfs(image, sr, sc, color, starting_pixel)
+        
+        return image
+        
+    def dfs(self, image, sr, sc, color, starting_pixel):
+            
+        if (sr < 0 or sr > len(image) - 1) or (sc < 0 or sc > len(image[0]) - 1) or (image[sr][sc] == color or image[sr][sc] != starting_pixel):
+                return
+        else:
+            image[sr][sc] = color
+            self.dfs(image, sr + 1, sc, color, starting_pixel)
+            self.dfs(image, sr - 1, sc, color, starting_pixel)
+            self.dfs(image, sr , sc + 1, color, starting_pixel)
+            self.dfs(image, sr , sc - 1, color, starting_pixel)
+        
+S = Solution()
+print(S.floodFill([[1,1,1],[1,1,0],[1,0,1]],1,1,2))

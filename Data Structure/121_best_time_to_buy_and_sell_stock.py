dfb60d8261dfced24f441ca0852b84f58faceb94
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-09-16 20:06:03 +0600

    Leetcode 121 solved

---
+++ Data Structure/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        maxP = 0
+        
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxP = max(maxP, profit)
+            else:
+                left = right
+            right += 1
+            
+        return maxP
+        
+        
+
+S = Solution()
+print(S.maxProfit([7,1,5,3,6,4]))

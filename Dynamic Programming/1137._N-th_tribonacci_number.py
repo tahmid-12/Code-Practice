71755749af8368aeb8d896a78f5ed64ab550e594
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-10-28 21:41:46 +0600

    leetcode 509 solved.

--- Dynamic Programming/1137._N-th_tribonacci_number.py
+++ Dynamic Programming/1137._N-th_tribonacci_number.py
@@ -2,15 +2,16 @@ from typing import List
 
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n==0:
-            return 0
-        if n==1:
+        if n <= 1:
+            return n
+        elif n == 2:
             return 1
         
-        if n==2:
-            return 1
-            
-        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        first, second, third = 0, 1, 1
+        for _ in range(3, n+1):
+            first, second, third = second, third, first + second + third
+        
+        return third
         
         
 S = Solution()

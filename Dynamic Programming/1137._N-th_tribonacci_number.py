1f93c8193fb0229ffb0e8a2ee77ff9ca60dbad3f
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-10-28 21:35:42 +0600

    Leetcode 1137 solved.

---
+++ Dynamic Programming/1137._N-th_tribonacci_number.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        
+        if n==2:
+            return 1
+            
+        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        
+        
+S = Solution()
+print(S.tribonacci(4))

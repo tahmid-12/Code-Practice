2dec56f26f3bf381afc5768a80b87921a5196614
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-08-09 01:10:30 +0600

    leetcode 1652 Solved

---
+++ Sliding Window/1652_Defuse_the_Bomb.js
@@ -0,0 +1,30 @@
+var decrypt = function(code, k) {
+     const n = code.length;
+    const result = new Array(n).fill(0);
+    if (k === 0) return result;
+
+    const arr = code.concat(code); 
+    let start, end;
+
+    if (k > 0) {
+        start = 1;       
+        end = k;         
+    } else {
+        start = n + k;   
+        end = n - 1;      
+        k = -k;          
+    }
+
+    let windowSum = 0;
+    for (let i = start; i <= end; i++) {
+        windowSum += arr[i];
+    }
+
+    for (let i = 0; i < n; i++) {
+        result[i] = windowSum;
+        windowSum -= arr[start++];
+        windowSum += arr[++end];  
+    }
+
+    return result;
+};

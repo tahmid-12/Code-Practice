83c57980b4007a7ac6bf0ec254c76baa51255726
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-08-23 23:04:20 +0600

    Leetcode 925 Solved

---
+++ Two Pointers/925. Long Pressed Name.js
@@ -0,0 +1,16 @@
+var isLongPressedName = function(name, typed) {
+    let i = 0;
+    let j = 0;
+
+    while(j < typed.length){
+        if(i < name.length && name[i] === typed[j]){
+            i++;
+            j++;
+        }else if(j > 0 && typed[j] === typed[j - 1]){
+            j++;
+        }else{
+            return false;
+        }
+    } 
+    return i == name.length
+};
